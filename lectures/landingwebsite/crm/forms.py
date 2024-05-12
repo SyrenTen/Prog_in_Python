3ef01251e595057a096020f0a63b4c5fd27a091f
Vitaliia <86183868+SyrenTen@users.noreply.github.com>
2024-05-12 15:15:25 +0200

    Create forms.py

---
+++ lectures/landingwebsite/crm/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class OrderForms(forms.Form):
+    name = forms.CharField(max_length=200, required=False, widget=forms.TextInput(attrs={'class': 'css_input'}))
+    phone = forms.CharField(max_length=200)
